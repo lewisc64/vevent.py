cf3eabdda1b6b0a400d0af9b8f1944bc1c37143f
redmechanic <redmechanic_@outlook.com>
2018-09-30 18:37:23 +0100

    fixed dead link

--- vevent.py
+++ vevent.py
@@ -5,7 +5,7 @@ import re
 vevent.py
 =========
 
-github: https://github.com/redmechanic/vevent.py
+github: https://github.com/lewisc64/vevent.py
 
 Extracts basic info on all events in an iCalendar file (.ics).
 For example, to get the summary of the first event on 2017-1-18:
